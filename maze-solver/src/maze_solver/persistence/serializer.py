91c64cf78a291af271caf8de4952abcf3108601c
Saish Shirodkar <shirodkarsaish00@gmail.com>
2023-11-22 10:58:59 +0530

    chnage

--- maze-solver/src/maze_solver/persistence/serializer.py
+++ maze-solver/src/maze_solver/persistence/serializer.py
@@ -1,10 +1,7 @@
 import array
 import pathlib
-# from typing import Iterator
 
-# from maze_solver.models.border import Border
 from maze_solver.models.maze import Maze
-# from maze_solver.models.role import Role
 from maze_solver.models.square import Square
 from maze_solver.persistence.file_format import FileBody, FileHeader
 
@@ -22,16 +19,5 @@ def serialize(maze: Maze) -> tuple[FileHeader, FileBody]:
     body = FileBody(array.array("B", map(compress, maze)))
     return header, body
 
-# def deserialize(header: FileHeader, body: FileBody) -> Iterator[Square]:
-#     for index, square_value in enumerate(body.square_values):
-#         row, column = divmod(index, header.width)
-#         border, role = decompress(square_value)
-#         yield Square(index, row, column, border, role)
-
-
 def compress(square: Square) -> int:
     return (square.role << 4) | square.border.value
-
-
-# def decompress(square_value: int) -> tuple[Border, Role]:
-#     return Border(square_value & 0xF), Role(square_value >> 4)
