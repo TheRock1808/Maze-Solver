c7a49f5b319b617123dfc43270d97869a57dba2e
Saish Shirodkar <shirodkarsaish00@gmail.com>
2023-11-22 22:43:19 +0530

    change main

--- maze-solver/src/maze_solver/__main__.py
+++ maze-solver/src/maze_solver/__main__.py
@@ -1,26 +1,51 @@
-# __main__.py
-
 import argparse
 import pathlib
-
+import tkinter as tk
+from tkinter import filedialog
 from maze_solver.graphs.solver import solve_all
 from maze_solver.models.maze import Maze
 from maze_solver.view.renderer import SVGRenderer
 
-def main() -> None:
-    maze = Maze.load(parse_path())
-    solutions = solve_all(maze)
-    if solutions:
-        renderer = SVGRenderer()
-        for solution in solutions:
-            renderer.render(maze, solution).preview()
-    else:
-        print("No solution found")
+class MazeSolverGUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Maze Solver")
 
-def parse_path() -> pathlib.Path:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("path", type=pathlib.Path)
-    return parser.parse_args().path
+        self.file_path = None
+
+        open_button = tk.Button(root, text="Open Maze File", command=self.open_file)
+        open_button.pack(pady=10)
+
+        solve_button = tk.Button(root, text="Solve Maze", command=self.solve_and_display)
+        solve_button.pack(pady=10)
+
+    def open_file(self):
+        file_path = filedialog.askopenfilename(filetypes=[("Maze files", "*.maze *.txt")])
+        if file_path:
+            self.file_path = pathlib.Path(file_path)
+            print(f"Selected maze file: {self.file_path}")
+
+    def solve_and_display(self):
+        if self.file_path:
+            maze = Maze.load(self.file_path)
+            solutions = solve_all(maze)
+
+            if solutions:
+                renderer = SVGRenderer()
+
+                renderer.render(maze).preview()
+
+                for solution in solutions:
+                    renderer.render(maze, solution).preview()
+            else:
+                print("No solution found")
+        else:
+            print("Please select a maze file first.")
+
+def main():
+    root = tk.Tk()
+    app = MazeSolverGUI(root)
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
--- maze-solver/src/maze_solver/app.py
+++ maze-solver/src/maze_solver/app.py
@@ -1,18 +1,23 @@
-from flask import Flask, render_template, request, jsonify
+# app.py
+from flask import Flask, render_template, jsonify
 from maze_solver.view.renderer import SVGRenderer
+from maze_solver.maze_solver_utils import solve_maze_and_get_html, parse_path
+import pathlib
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Call the function from maze_solver_utils to get the HTML content
+    maze_path = pathlib.Path("C:/Users/HP/OneDrive/Documents/CODE/GT_Project/mazes/miniature.maze")
+    html_content = solve_maze_and_get_html(maze_path)
+    return render_template('index.html', maze_html=html_content)
 
 @app.route('/solve_maze', methods=['POST'])
 def solve_maze():
     # Here you would implement your maze-solving logic
     # For simplicity, let's just return a dummy solution for now
     solution = [[0, 1, 0], [0, 1, 0], [0, 0, 0]]
-
     return jsonify({'solution': solution})
 
 if __name__ == '__main__':
